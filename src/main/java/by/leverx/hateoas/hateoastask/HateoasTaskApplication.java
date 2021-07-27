8f81bb65a398b29c5faad0165509ca36c50166bd
Valery1508 <lera.zubritskaya@gmail.com>
2021-07-27 15:53:12 +0300

    initial comit

---
+++ src/main/java/by/leverx/hateoas/hateoastask/HateoasTaskApplication.java
@@ -0,0 +1,13 @@
+package by.leverx.hateoas.hateoastask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HateoasTaskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HateoasTaskApplication.class, args);
+    }
+
+}
---
+++ src/test/java/by/leverx/hateoas/hateoastask/HateoasTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package by.leverx.hateoas.hateoastask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HateoasTaskApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
